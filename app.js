941628dba2d8d699b39f229bb8b74623f412b711
Adam Kliment <adam@apiary.io>
2015-04-15 10:11:36 -0700

    Fixed status code and blueprint

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var app = require('express')();
 port = process.env.PORT || 3000
 
 //app.post('/rescue', function (req, res) {
-//  res.json({"message": "You will live!"});
-//})
+//  res.status(201).json({"message": "You will live!"});
+//});
 
 var server = app.listen(port)
 console.log('Listening on http://localhost:' + port);
