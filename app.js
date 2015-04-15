d7f951f9489472c555d84d763106bcf58f90146c
Adam Kliment <adam@apiary.io>
2015-04-15 08:55:18 -0700

    Port from env var

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 var app = require('express')();
 
+port = process.env.PORT || 3000
+
 //app.post('/rescue', function (req, res) {
 //  res.json({"message": "You will live!"});
 //})
 
-var server = app.listen(3000);
-console.log('Listening on http://localhost:3000');
+var server = app.listen(port)
+console.log('Listening on http://localhost:' + port);
