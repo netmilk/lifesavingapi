8ed702a08dd9777eb77c56a9bb0c46265ac36a84
Adam Kliment <adam@apiary.io>
2015-04-15 08:48:11 -0700

    Added empty application

---
+++ app.js
@@ -0,0 +1,8 @@
+var app = require('express')();
+
+//app.post('/rescue', function (req, res) {
+//  res.json({"message": "You will live!"});
+//})
+
+var server = app.listen(3000);
+console.log('Listening on http://localhost:3000');
