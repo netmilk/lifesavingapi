aecb1f3f6dd10aca6df9d550eeaa9e6e25da33f3
Adam Kliment <adam@apiary.io>
2015-04-16 15:57:22 -0700

    Passing

--- app.js
+++ app.js
@@ -2,9 +2,9 @@ var app = require('express')();
 
 port = process.env.PORT || 3000
 
-//app.post('/912', function (req, res) {
-//  res.status(201).json({"message": "You will live!"});
-//});
+app.post('/912', function (req, res) {
+  res.status(201).json({"message": "You will live!"});
+});
 
 var server = app.listen(port)
 console.log('Listening on http://localhost:' + port);
