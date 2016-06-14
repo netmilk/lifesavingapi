139b973ae8501578d4c1b74cfa10b32c88560d40
Adam Kliment <adam@apiary.io>
2016-06-14 21:47:13 +0200

    Swagger experiment

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ var app = require('express')();
 
 port = process.env.PORT || 3000
 
-//app.post('/112', function (req, res) {
-//  res.status(201).json({"message": "You will live!"});
-//});
+app.post('/112', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(201);
+  res.send(new Buffer(JSON.stringify({"message": "You will live!"})));
+});
 
 var server = app.listen(port)
 console.log('Listening on http://localhost:' + port);
